97c6ae8ba6b4e77ac69ed3dabe808381d21c2312
yoryi <j_leandro_vaskez@hotmail.com>
2024-10-06 16:19:22 -0500

    fixed

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const dotenv = require('dotenv');
 const evaluacionRoutes = require('./routes/evaluacionRoutes');
 const notaRoutes = require('./routes/notaRoutes');
 
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 27017;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar a MongoDB', err));
 
